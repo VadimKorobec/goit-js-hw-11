bc77d7dae7a1b75deb10c328ccc94edd77e7bbc0
VadimKorobec <_s_e_t__@ukr.net>
2022-12-03 16:34:44 +0200

    Update fetch-images.js

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 export { fetchImages };
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = `https://pixabay.com/api/`;
 const KEY = '31618598-dd0b87f36bc5180b6dfd99237';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&horizontal=true&page=${page}&per_page=${perPage}`
   );
+
   return response;
 }
